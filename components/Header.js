0af5de8c294e2e26432f26dc00b91fc9a76d6e37
mandi andrade <mandi.andrade20@gmail.com>
2023-08-08 18:21:48 -0500

    added logo and edited blog name

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      {/* <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
+      <Image
+        src="/images/gyst_logo.jpeg"
+        width={120}
+        height={120}
+        alt="Gyst Rewind logo"
+      />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
