238f64cee69f64cf3f27d5faf7f320cfa5b13ed8
Mandi Girasol <118700217+min-dles@users.noreply.github.com>
2024-01-27 05:18:13 +0000

    removed extra code, test strings

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,13 +1,6 @@
 import { getGlobalData } from '../../utils/global-data';
-import {
-  getNextPostBySlug,
-  getPostBySlug,
-  getPreviousPostBySlug,
-  postFilePaths,
-} from '../../utils/mdx-utils';
 import gystLogo from '../../public/images/gyst_logo.jpeg';
 
-import { MDXRemote } from 'next-mdx-remote';
 import Head from 'next/head';
 import Link from 'next/link';
 import ArrowIcon from '../../components/ArrowIcon';
@@ -31,48 +24,49 @@ const components = {
 };
 
 export default function PostPage({
-  source,
-  frontMatter,
-  prevPost,
-  nextPost,
   globalData,
+  currentEpisode,
+  previousEpisode,
+  nextEpisode,
 }) {
+
   return (
     <Layout>
       <SEO
-        title={`${frontMatter.title} - ${globalData.name}`}
-        description={frontMatter.description}
+        title={`${currentEpisode.title} - ${globalData.name}`}
+        description={currentEpisode.description}
       />
       <Header name={globalData.name} />
       <article className="px-6 md:px-0">
         <header>
           <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">
-            {frontMatter.title}
+            {currentEpisode.title}
           </h1>
-          {frontMatter.description && (
-            <p className="text-xl mb-4">{frontMatter.description}</p>
-          )}
         </header>
         <main>
           <article className="prose dark:prose-dark">
-            <MDXRemote {...source} components={components} />
+            {currentEpisode.description && (
+              <p
+                className="text-xl mb-4"
+                dangerouslySetInnerHTML={{ __html: currentEpisode.description }}
+              />
+            )}
           </article>
         </main>
         <div className="grid md:grid-cols-3 lg:-mx-24 mt-12">
-          {prevPost && (
-            <Link href={`/posts/${prevPost.slug}`}>
+          {previousEpisode && (
+            <Link href={`/posts/${previousEpisode.id}`}>
               <a className="py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col">
                 <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
                   Previous
                 </p>
                 <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
-                  {prevPost.title}
+                  {previousEpisode.title}
                 </h4>
                 <ArrowIcon className="transform rotate-180 mx-auto md:mr-0 mt-auto" />
               </a>
             </Link>
           )}
-          {/* home navigation being added here:  */}
           <Link href="/">
             <a className="py-8 px-10 text-center md:text-center md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
               <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
@@ -81,17 +75,22 @@ export default function PostPage({
               <Image
                 src={gystLogo}
                 alt="Gyst Rewind logo"
+                sizes="100vw"
+                style={{
+                  width: '50%',
+                  height: 'auto',
+                }}
               />
             </a>
           </Link>
-          {nextPost && (
-            <Link href={`/posts/${nextPost.slug}`}>
+          {nextEpisode && (
+            <Link href={`/posts/${nextEpisode.id}`}>
               <a className="py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
                 <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
                   Next
                 </p>
                 <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
-                  {nextPost.title}
+                  {nextEpisode.title}
                 </h4>
                 <ArrowIcon className="mt-auto mx-auto md:ml-0" />
               </a>
@@ -114,27 +113,42 @@ export default function PostPage({
 
 export const getStaticProps = async ({ params }) => {
   const globalData = getGlobalData();
-  const { mdxSource, data } = await getPostBySlug(params.slug);
-  const prevPost = getPreviousPostBySlug(params.slug);
-  const nextPost = getNextPostBySlug(params.slug);
+  const items = await fetch(`https://www.buzzsprout.com/api/${process.env.PODCAST_ID}/episodes.json`, {
+    method: 'GET',
+    withCredentials: true,
+    credentials: 'include',
+    headers: {
+      "Authorization": `Token token=${process.env.API_KEY}`
+    }
+  })
+  const res = await items.json()
+  const currentEpisode = res.find(episode => (params.slug === String(episode.id)));
+  const allIdsAsStrings = res.map(({ id }) => `${id}`);
+  const currentEpisodeIndex = allIdsAsStrings.indexOf(params.slug);
+  const previousEpisode = res[currentEpisodeIndex + 1] || null;
+  const nextEpisode = res[currentEpisodeIndex - 1] || null;
 
   return {
     props: {
       globalData,
-      source: mdxSource,
-      frontMatter: data,
-      prevPost,
-      nextPost,
+      currentEpisode,
+      previousEpisode,
+      nextEpisode,
     },
   };
 };
 
 export const getStaticPaths = async () => {
-  const paths = postFilePaths
-    // Remove file extensions for page paths
-    .map((path) => path.replace(/\.mdx?$/, ''))
-    // Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }));
+  const items = await fetch(`https://www.buzzsprout.com/api/${process.env.PODCAST_ID}/episodes.json`, {
+    method: 'GET',
+    withCredentials: true,
+    credentials: 'include',
+    headers: {
+      "Authorization": `Token token=${process.env.API_KEY}`
+    }
+  });
+  const res = await items.json();
+  const paths = res.map((episode) => ({ params: { slug: episode.id.toString() } }));
 
   return {
     paths,
