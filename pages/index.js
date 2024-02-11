867857db85d8c7ba6e49425a49602b4326b1a0dd
mandi <118700217+min-dles@users.noreply.github.com>
2024-02-11 15:18:24 -0600

    Feature/get buzzsprout posts (#11)
    * linter errors
    
    * buzzsprout domain on next images
    
    * ignore plain env files
    
    * add devcontainer
    
    * pass allEpisodes from buzzsprout & fix gif import
    
    * formatted allEpisodes per template
    
    * removed extra code, test strings
    
    * fixed classes on mobile styling
    
    * fixed hydration issue by writing ArticleText component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { getPosts } from '../utils/mdx-utils';
 import Image from 'next/image';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -8,14 +7,16 @@ import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 import styles from '../styles/player.module.css'
+import AnimatedLogo from '/public/images/gyst_loop.gif'
+import ArticleText from '../components/ArticleText';
 
-export default function Index({ posts, globalData }) {
+export default function Index({ globalData, allEpisodes }) {
 
   return (
     <Layout>
-      <div className={`${styles.wrapper} before:bg-white before:bg-white before:backdrop-blur-lg before:dark:bg-black before:dark:bg-opacity-30 before:bg-opacity-10 `}>
+      <div className={`${styles.wrapper} before:bg-white before:backdrop-blur-lg before:dark:bg-black before:dark:bg-opacity-30 before:bg-opacity-10 `}>
         <Image
-          src="/../public/images/gyst_loop.gif"
+          src={AnimatedLogo}
           width={500}
           height={420}
           alt="Gyst Rewind logo"
@@ -25,7 +26,7 @@ export default function Index({ posts, globalData }) {
       <Header name={globalData.name} />
       <main className="w-full">
         <h1
-          className="text-3xl lg:text-5xl text-center mb-12 font-extrabold text-transparent text-8xl bg-clip-text"
+          className="text-3xl lg:text-5xl text-center mb-12 font-extrabold text-transparent bg-clip-text"
           style={{
             height: 'fit-content',
             lineHeight: '1.0',
@@ -35,32 +36,35 @@ export default function Index({ posts, globalData }) {
           {globalData.blogTitle}
         </h1>
         <ul className="w-full">
-          {posts.map((post) => (
+          <ArticleText>
+          {allEpisodes.map(episode => (
             <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
+            key={episode.id}
+            className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
             >
               <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
+                as={`/posts/${episode.id}`}
                 href={`/posts/[slug]`}
-              >
+                >
                 <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
+                  {episode.published_at && (
                     <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
+                      {`${(new Date (episode.published_at)).toLocaleDateString()}`}
                     </p>
                   )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
+                  <h2 className="text-2xl md:text-3xl">{episode.title}</h2>
+                  {episode.description && (
+                    <div
+                    className="mt-3 text-lg opacity-60"
+                    dangerouslySetInnerHTML={{ __html: episode.description.split("<br>")[0] }}
+                    />
+                    )}
                   <ArrowIcon className="mt-4" />
                 </a>
               </Link>
             </li>
           ))}
+          </ArticleText>
         </ul>
       </main>
       <Footer copyrightText={globalData.footerText} />
@@ -76,9 +80,17 @@ export default function Index({ posts, globalData }) {
   );
 }
 
-export function getStaticProps() {
-  const posts = getPosts();
+export async function getStaticProps() {
+  const items = await fetch(`https://www.buzzsprout.com/api/${process.env.PODCAST_ID}/episodes.json`, {
+    method: 'GET',
+    withCredentials: true,
+    credentials: 'include',
+    headers: {
+      "Authorization": `Token token=${process.env.API_KEY}`
+    }
+  })
+  const res = await items.json()
   const globalData = getGlobalData();
 
-  return { props: { posts, globalData } };
+  return { props: { globalData, allEpisodes: res } };
 }
