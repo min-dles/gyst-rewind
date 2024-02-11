a8e06f824f4fbacfc478ef341178edf1409288a7
Mandi Girasol <118700217+min-dles@users.noreply.github.com>
2024-02-11 20:48:55 +0000

    fixed classes on mobile styling

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -47,7 +47,7 @@ export default function PostPage({
           <article className="prose dark:prose-dark">
             {currentEpisode.description && (
               <p
-                className="text-xl mb-4"
+                className="text-xl mb-4 px-2 [&_p]:break-all [&_p]:text-pretty md:[&_p]:break-keep"
                 dangerouslySetInnerHTML={{ __html: currentEpisode.description }}
               />
             )}
@@ -68,19 +68,20 @@ export default function PostPage({
             </Link>
           )}
           <Link href="/">
-            <a className="py-8 px-10 text-center md:text-center md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
+            <a className="py-8 px-10 text-center md:text-center md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col items-center">
               <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
                 Home
               </p>
-              <Image
-                src={gystLogo}
-                alt="Gyst Rewind logo"
-                sizes="100vw"
-                style={{
-                  width: '50%',
-                  height: 'auto',
-                }}
-              />
+              <div className="w-16 h-16 rounded-full">
+                <Image
+                  src={gystLogo}
+                  alt="Gyst Rewind logo"
+                  className="rounded-full"
+                  width={64}
+                  height={64}
+                  objectFit='contain'
+                />
+              </div>
             </a>
           </Link>
           {nextEpisode && (
